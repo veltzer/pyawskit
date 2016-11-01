3f8933a9b45e3a6b3046fb3a4d2a5a897d9755f8
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-01 18:37:52 +0200

    this is starting to run

--- boot.py
+++ boot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 '''
 A python version of the script here:
@@ -16,49 +16,86 @@ A python version of the script here:
 - configure vim to do correct python editing and save editing positions.
 '''
 
-import subprocess
-import os
-import sys
+import subprocess # for check_output, check_call
+import os # for geteuid
+import sys # for exit
+import enum  # for Enum
+from typing import List
 
-# check that we are running as root
-if not os.geteuid() == 0:
-    sys.exit('Script must be run as root')
+class OSType(enum.Enum):
+    ubuntu=1
+    aml=2
 
-# first find out if we are on ubuntu or amazon linux
+def run(args: List[str]):
+    #print('running', args)
+    subprocess.check_call(
+        args,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
 
 os_type=None
-try:
-    if subprocess.check_output(['lsb_release','--id','-s']).rstrip()=='Ubuntu':
-        os_type='ubuntu'
-except:
-    pass
-try:
-    with open('/etc/issue') as f:
-        d=f.read()
-        if d.startswith('Amazon Linux AMI'):
-            os_type='amazon_linux'
-except:
-    pass
+# first find out if we are on ubuntu or amazon linux
+def detect_os():
+    global os_type
+    try:
+        if subprocess.check_output(['lsb_release','--id','-s']).decode().rstrip()=='Ubuntu':
+            os_type=OSType.ubuntu
+    except:
+        pass
+    try:
+        with open('/etc/issue') as f:
+            d=f.read()
+            if d.startswith('Amazon Linux AMI'):
+                os_type=OSType.aml
+    except:
+        pass
 
-if os_type is None:
-    sys.exit('could not detect the os running')
+    if os_type is None:
+        sys.exit('could not detect the os running')
+
+def is_os_type(t):
+    global os_type
+    return os_type == t
+
+# check that we are running as root
+def check_root():
+    if not os.geteuid() == 0:
+        sys.exit('script must be run as root')
+
+def update_machine():
+    if is_os_type(OSType.ubuntu):
+        run(['apt-get','update'])
+        run(['apt-get','upgrade'])
+    if is_os_type(OSType.aml):
+        run(['yum','update'])
+        run(['yum','upgrade'])
+
+
+check_root()
+detect_os()
+update_machine()
+
+####### fix the code from here #######
 
 # install neccessary package per platform
 
-list_of_packages_ubuntu=[
+list_of_packages={
+    OSType.ubuntu: [
         'python-pip',
         'python-dev',
-	'python3-pip',
-	'python3-dev',
-	'git',
-	'parallel',
-	'jq',
-	'tree',
-	'zip',
-]
+        'python3-pip',
+        'python3-dev',
+        'git',
+        'parallel',
+        'jq',
+        'tree',
+        'zip',
+    ],
+    OSType.aml: [
+    ],
+}
 
-list_of_package_aml=[
-]
-
+# install the packages
 #subprocess.check_call([
-#)
+#])
