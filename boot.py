2fcafd4a829c1e77191a687071d2cabd46d28255
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-01 14:36:39 +0200

    first version of the script

---
+++ boot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+'''
+A python version of the script here:
+-https://gist.github.com/joemiller/6049831
+
+- apt-get update
+- apt-get dist-upgrade
+- install a list of packages on the system:
+	pip
+	git
+	parallel
+	jq
+	python-dev
+	python3-pip
+	python3-dev
+	tree
+	zip
+- format and mount all disks
+- copy ~/.aws to it
+- copy keys from ~/.ssh to it
+- configure vim to do correct python editing and save editing positions.
+- copy ~/.gitconfig to it.
+- set the hostname of the machine (it's usualy hostname is ip-XXX-XXX-XXX-XXX).
+'''
