d68f91b992113a6fa6edb205d794e880e13a2818
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-02 17:58:06 +0200

    removed extra cruft

--- setup.py
+++ setup.py
@@ -1,125 +1,36 @@
-"""A setuptools based setup module.
+"""
+A setuptools based setup module.
 
 See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
-import sys # for exit
-import os # for walk
+import setuptools
 
-here = path.abspath(path.dirname(__file__))
-
-'''
-def find_packages(src_folder):
-    ret={}
-    for root, dirs, files in os.walk(src_folder): 
-        for dir in dirs:
-            full = os.path.join(root, dir)
-            ret[full]=full
-    print(ret)
-    sys.exit(1)
-'''
-
-# Get the long description from the README file
-#with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-#    long_description = f.read()
-
-setup(
+setuptools.setup(
     name='awskit',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version='0.0.8',
-
     description='awskit is a collection of utilities to help interact with aws',
     long_description='this is the long description of awskit',
-
-    # The project's main homepage.
-    #url='git@github.com:veltzer/awskit.git',
     url='https://veltzer.github.io/awskit',
-
-    # Author details
     author='Mark Veltzer',
     author_email='mark.veltzer@gmail.com',
-
-    # Choose your license
     license='GPL3',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        #'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
     ],
-
-    # What does your project relate to?
     keywords='aws utils',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     package_dir={'':'src'},
-    #packages=map(lambda x: os.path.join('src', x), find_packages('src')),
-    packages=find_packages('src'),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
+    packages=setuptools.find_packages('src'),
     install_requires=[
         'boto',
         'boto3',
     ],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    #extras_require={
-    #    'dev': [],
-    #    'test': [],
-    #},
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'sample': ['package_data.dat'],
-    #},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
             'prep_machine=awskit.prep_machine:main',
