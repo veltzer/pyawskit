09627d6e8a2375d91dc87bbb3d80f0f9821ee526
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-02 13:16:31 +0200

    notes about what to do

--- scripts/register.py
+++ scripts/register.py
@@ -2,6 +2,15 @@
 
 '''
 This script registers your project via twine.
+
+It does:
+- full clean
+- build wheel using setup.py
+- twine register
+- full clean
+
+References:
+- https://packaging.python.org/distributing/
 '''
 
 import common # for git_clean_full
