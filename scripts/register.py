270d42129678008044cf61465fa28e5e75ade077
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-02 13:30:51 +0200

    package is registered

--- scripts/register.py
+++ scripts/register.py
@@ -10,11 +10,17 @@ It does:
 - full clean
 
 NOTE!!!
-- It seems that registering packages is no longer supported. Just upload the package.
+- It seems that registering packages is no longer supported via twine. Just upload the package.
 This is the error that I got:
 Registering package to https://upload.pypi.org/legacy/
 Registering awskit-0.0.1-py2.py3-none-any.whl
 HTTPError: 410 Client Error: This API is no longer supported, instead simply upload the file.
+
+So to register you need to:
+python setup.py register -r pypi
+
+registering the same package many times works.
+You just need to do it once...:)
 
 References:
 - https://packaging.python.org/distributing/
@@ -22,9 +28,21 @@ References:
 
 import common # for git_clean_full
 import subprocess # for check_call, DEVNULL
-import os # for listdir
 
 common.git_clean_full()
+subprocess.check_call([
+    'python',
+    'setup.py',
+    'register',
+    '-r',
+    'pypi',
+], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+common.git_clean_full()
+
+'''
+import os # for listdir
+
+this does not work
 subprocess.check_call([
     'python3',
     'setup.py',
@@ -41,5 +59,5 @@ subprocess.check_call([
     'register',
     full_filename,
 ])
-#], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-#common.git_clean_full()
+], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+'''
