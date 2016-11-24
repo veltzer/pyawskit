c9ea78e9fbf28e84821fd96338956e7fb02072f2
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-24 13:56:24 +0200

    better registration script

--- scripts/common.py
+++ scripts/common.py
@@ -1,11 +1,24 @@
 import subprocess # for check_call
 import os.path # for expanduser
+import sys # for exit
 
 config_file=os.path.expanduser('~/.pypirc')
 
+def check_call_no_output(args):
+    p = subprocess.Popen(
+            args,
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE,
+    )
+    (res_stdout, res_stderr) = p.communicate()
+    if p.returncode:
+        print(res_stdout, end='')
+        print(res_stderr, end='')
+        sys.exit(p.returncode)
+
 def git_clean_full():
-    subprocess.check_call([
+    check_call_no_output([
         'git',
         'clean',
         '-qffxd',
-    ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    ])
--- scripts/register.py
+++ scripts/register.py
@@ -1,63 +1,64 @@
 #!/usr/bin/python3
 
 '''
-This script registers your project via twine.
+This script registers your project in pypi.
 
-It does:
+when registering via twine(1) you need to:
 - full clean
 - build wheel using setup.py
 - twine register
 - full clean
+This method works, if you register twice it is ok.
+You just need to do it once...:)
 
-NOTE!!!
-- It seems that registering packages is no longer supported via twine. Just upload the package.
-This is the error that I got:
-Registering package to https://upload.pypi.org/legacy/
-Registering awskit-0.0.1-py2.py3-none-any.whl
-HTTPError: 410 Client Error: This API is no longer supported, instead simply upload the file.
-
-So to register you need to:
-python setup.py register -r pypi
-
+when registering via setup.py you need to:
+- full clean
+- python setup.py register -r pypi
+- full clean
 registering the same package many times works.
 You just need to do it once...:)
 
 References:
 - https://packaging.python.org/distributing/
+
+TODO:
+- check if I'm already registered and don't register if that is the case.
 '''
 
 import common # for git_clean_full
 import subprocess # for check_call, DEVNULL
-
-common.git_clean_full()
-subprocess.check_call([
-    'python',
-    'setup.py',
-    'register',
-    '-r',
-    'pypi',
-], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-common.git_clean_full()
-
-'''
 import os # for listdir
 
-this does not work
-subprocess.check_call([
-    'python3',
-    'setup.py',
-    'bdist_wheel',
-], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+do_use_setup = False
+do_use_twine = True
+    
+common.git_clean_full()
 
-# at this point there should be only one file in the 'dist' folder
-file_list = list(os.listdir('dist'))
-assert len(file_list) == 1
-filename = file_list[0]
-full_filename = os.path.join('dist', filename)
-subprocess.check_call([
-    'twine',
-    'register',
-    full_filename,
-])
-], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-'''
+if do_use_setup:
+    common.check_call_no_output([
+        'python',
+        'setup.py',
+        'register',
+        '-r',
+        'pypi',
+    ])
+
+if do_use_twine:
+    common.check_call_no_output([
+        'python3',
+        'setup.py',
+        'bdist_wheel',
+    ])
+
+    # at this point there should be only one file in the 'dist' folder
+    file_list = list(os.listdir('dist'))
+    assert len(file_list) == 1
+    filename = file_list[0]
+    full_filename = os.path.join('dist', filename)
+    common.check_call_no_output([
+        'twine',
+        'register',
+        full_filename,
+    ])
+
+common.git_clean_full()
