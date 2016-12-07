92e5ba92e4201fc921c80b8161a7f533b147180e
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-07 17:55:45 +0200

    fix problems with the code

--- src/awskit/generate_ssh_config.py
+++ src/awskit/generate_ssh_config.py
@@ -5,10 +5,10 @@ Notice that you must hold all of your .pem files in ~/.aws/keys
 
 import os.path
 import boto3
-import boto3.resources
+import boto3.resources.factory
 import ujson
 import sys
-from typing import List
+
 
 pattern = """Host {host}
 \tHostName {public_ip}
@@ -21,12 +21,9 @@ def main():
     comment_line = "# DO NOT EDIT BEYOND THIS LINE - LEAVE THIS LINE AS IS\n"
     config_file = os.path.expanduser("~/.ssh/config")
     filter_file = os.path.expanduser("~/.ssh/aws_filter")
-    # no need to expand this as ~/.ssh/config can work with ~...
-    pem_pattern = "~/.awk/keys/{key_name}.pem"
 
     with open(filter_file) as file_handle:
         filters = ujson.loads(file_handle.read())
-    # print(filters)
 
     with open(config_file) as file_handle:
         lines = file_handle.readlines()
@@ -37,18 +34,19 @@ def main():
         lines = lines[:location_of_comment_line]
     except ValueError:
         pass
-    # print("lines is {}".format(lines), file=sys.stderr)
 
+    # look for servers matching the query
     ec2 = boto3.resource('ec2')
-    instances = list(ec2.instances.filter(Filters=filters))  # type: List[boto3.resources.collection.ec2.instancesCollection]
+    instances = list(ec2.instances.filter(Filters=filters))
     num_of_instances = len(instances)
     print("Found {} instances".format(num_of_instances), file=sys.stderr)
     assert num_of_instances > 0
 
+    # add the comment line
     lines.append(comment_line)
 
     # add bunch of lines for each server
-    for instance in instances:  # type: boto3.resources.factory.ec2.Instance
+    for instance in instances:
         tags_dict = {}
         for tag in instance.tags:
             tags_dict[tag["Key"]] = tag["Value"]
@@ -58,7 +56,6 @@ def main():
             key_name=instance.key_name,
         )
         lines.extend(pattern_to_add)
-    # print("lines is {}".format(lines), file=sys.stderr)
 
     # print the final lines to the config file
     with open(config_file, "wt") as file_handle:
