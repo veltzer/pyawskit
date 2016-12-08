04d573332d75eb9819d24d1f24c6f63239c6a4a8
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-08 13:58:13 +0200

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import setuptools
 
 setuptools.setup(
     name='awskit',
-    version='0.1.3',
+    version='0.1.4',
     description='awskit is a collection of utilities to help interact with aws',
     long_description='awskit helps you with various aws tasks',
     url='https://veltzer.github.io/awskit',
