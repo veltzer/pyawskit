2ee0162dd7c6ca6bc1e60e6275337e68fb5f713d
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-08 13:24:35 +0200

    fixed up some of the code

--- src/awskit/ak_prep_machine.py
+++ src/awskit/ak_prep_machine.py
@@ -1,4 +1,4 @@
-'''
+"""
 A python version of the bash script here:
 https://gist.github.com/joemiller/6049831
 
@@ -7,72 +7,83 @@ https://gist.github.com/joemiller/6049831
 - a reboot may be necessary if a new kernel was installed
 - install a list of packages on the system:
 - format and mount all disks
-    use madm
+    use mdadm
 - copy ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs to it
-- set the hostname of the machine (it's usualy hostname is ip-XXX-XXX-XXX-XXX).
+- set the hostname of the machine (it's usually hostname is ip-XXX-XXX-XXX-XXX).
 - configure vim to do correct python editing and save editing positions.
-- install fancy propmt on it.
+- install fancy prompt on it.
 - put the git repositories you want on it.
-'''
+"""
 
-import subprocess # for check_output, check_call
-import os # for geteuid
-import sys # for exit
-import enum  # for Enum
+import subprocess
+import os
+import sys
+import enum
 from typing import List
 
+
 class OSType(enum.Enum):
-    ubuntu=1
-    aml=2
+    ubuntu = 1
+    aml = 2
+
 
 def run(args: List[str]):
-    #print('running', args)
+    # print('running', args)
     subprocess.check_call(
         args,
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
 
-os_type=None
+
+os_type = None
+
+
 # first find out if we are on ubuntu or amazon linux
 def detect_os():
     global os_type
+    # noinspection PyBroadException
     try:
-        if subprocess.check_output(['lsb_release','--id','-s']).decode().rstrip()=='Ubuntu':
-            os_type=OSType.ubuntu
+        if subprocess.check_output(['lsb_release', '--id', '-s']).decode().rstrip() == 'Ubuntu':
+            os_type = OSType.ubuntu
     except:
         pass
+    # noinspection PyBroadException
     try:
         with open('/etc/issue') as f:
-            d=f.read()
+            d = f.read()
             if d.startswith('Amazon Linux AMI'):
-                os_type=OSType.aml
+                os_type = OSType.aml
     except:
         pass
 
     if os_type is None:
         sys.exit('could not detect the os running')
 
+
 def is_os_type(t):
     global os_type
     return os_type == t
+
 
 # check that we are running as root
 def check_root():
     if not os.geteuid() == 0:
         sys.exit('script must be run as root')
 
+
 def update_machine():
     if is_os_type(OSType.ubuntu):
-        run(['apt-get','update'])
-        run(['apt-get','upgrade'])
+        run(['apt-get', 'update'])
+        run(['apt-get', 'upgrade'])
     if is_os_type(OSType.aml):
-        run(['yum','update'])
-        run(['yum','upgrade'])
+        run(['yum', 'update'])
+        run(['yum', 'upgrade'])
 
-# install neccessary package per platform
+
+# install necessary package per platform
 def install_packages():
-    list_of_packages={
+    list_of_packages = {
         OSType.ubuntu: [
             'python-pip',
             'python-dev',
@@ -85,18 +96,24 @@ def install_packages():
             'zip',
             'awscli',
             's3cmd',
-            'mdadm', # is already installed on ubuntu on aws
-            'lrzip', # for lrzip(1), lrunzip(1)
-            'bc', # nice to have tool
-            's3fs', # s3 file system
-            'apt-file', # for finding package names by files
+            'mdadm',  # is already installed on ubuntu on aws
+            'lrzip',  # for lrzip(1), lrunzip(1)
+            'bc',  # nice to have tool
+            's3fs',  # s3 file system
+            'apt-file',  # for finding package names by files
         ],
         OSType.aml: [
         ],
     }
     # install the packages
-    #subprocess.check_call([
-    #])
+    args = [
+        "sudo",
+        "apt-get",
+        "install",
+    ]
+    args.extend(list_of_packages[OSType.ubuntu])
+    subprocess.check_call(args)
+
 
 def main():
     check_root()
@@ -104,5 +121,6 @@ def main():
     update_machine()
     install_packages()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
