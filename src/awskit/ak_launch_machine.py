e9ed0dee64be6dc40f335222038812ada14ddd86
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-27 13:22:52 +0200

    fixed all pycharm errors

--- src/awskit/ak_launch_machine.py
+++ src/awskit/ak_launch_machine.py
@@ -4,7 +4,11 @@
 This script launches a new machine via boto3
 
 default type: c3.2xlarge
+
+References:
+- http://boto3.readthedocs.io/en/latest/reference/services/ec2.html#EC2.Client.run_instances
 """
+
 
 def main():
     pass
