4194397a810fc1684fc5ceb856c08519ca0709c3
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-27 13:52:16 +0200

    pipegzip factored out

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setuptools.setup(
         'boto3',  # used by ak_generate_ssh_config
         'mount',  # used by ak_unify_disks
         'ujson',  # used by ak_generate_ssh_config
+        'pipegzip',  # used by ak_compress_s3_folder
     ],
     entry_points={
         'console_scripts': [
