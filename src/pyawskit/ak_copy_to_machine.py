33b8c452853a29700b8f8fe18f0c90cdfe705756
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-16 10:49:29 +0200

    refactor code

--- src/pyawskit/ak_copy_to_machine.py
+++ src/pyawskit/ak_copy_to_machine.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 """
-This script copies important files to a machine
-
-something like:
-copy ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs to it
+This script copies important files to a machine like key files and such.
 """
 
 
@@ -12,21 +9,27 @@ import subprocess
 import sys
 import os.path
 
-folders_to_copy = [
-    "~/.aws",
-    "~/.ssh",
-    "~/.s3cfg",
-    "~/.gitconfig",
-    "~/.passwd-s3fs",
-]
 
-assert len(sys.argv) == 2
-machine_name = sys.argv[1]
-args = [
-    "scp",
-    "-r",
-]
-args.extend(map(os.path.expanduser, folders_to_copy))
-args.append("ubuntu@{}:~".format(machine_name))
-# subprocess.check_call(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-subprocess.check_call(args)
+def main():
+    folders_to_copy = [
+        "~/.aws",
+        "~/.ssh",
+        "~/.s3cfg",
+        "~/.gitconfig",
+        "~/.passwd-s3fs",
+    ]
+
+    assert len(sys.argv) == 2
+    machine_name = sys.argv[1]
+    args = [
+        "scp",
+        "-r",
+    ]
+    args.extend(map(os.path.expanduser, folders_to_copy))
+    args.append("{machine_name}:~".format(machine_name=machine_name))
+    # subprocess.check_call(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.check_call(args)
+
+
+if __name__ == '__main__':
+    main()
