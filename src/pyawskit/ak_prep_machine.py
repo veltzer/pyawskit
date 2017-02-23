a6dde48b92f151cf685ecd0c289de634903f0797
Mark Veltzer <mark.veltzer@gmail.com>
2017-02-23 14:19:19 +0200

    more stuff to copy

--- src/pyawskit/ak_prep_machine.py
+++ src/pyawskit/ak_prep_machine.py
@@ -15,7 +15,7 @@ bash
 - install a list of packages on the system:
 - format and mount all disks
     use mdadm
-- copy ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs to it
+- scp -r ~/.config/pip/pip.conf ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs MarksHome:~
 - set the hostname of the machine (it's usually hostname is ip-XXX-XXX-XXX-XXX).
      $ sudo sh -c "echo MarksHome  > /etc/hostname"
 - set the hostname in /etc/hosts beside
