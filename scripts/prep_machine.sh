495bc22ba2fcdf15b1cf65cffcefac24925ad760
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-03 13:55:18 +0300

    more packages

--- scripts/prep_machine.sh
+++ scripts/prep_machine.sh
@@ -6,7 +6,7 @@ sudo apt -y update
 sudo apt -y dist-upgrade
 sudo apt -y auto-remove
 # a reboot may be necessary if a new kernel was installed
-sudo apt -y install python-pip python-dev python3-pip python3-dev git apt-file awscli s3cmd s3fs zip tree jq parallel mdadm lrzip bc sqlite3 pipemeter awscli postgresql-client-common postgresql-client-9.5
+sudo apt -y install python-pip python-dev python3-pip python3-dev git apt-file awscli s3cmd s3fs zip tree jq parallel mdadm lrzip bc sqlite3 pipemeter awscli postgresql-client-common postgresql-client-9.5 virtualenv virtualenvwrapper
 sudo -H pip install pip --upgrade
 sudo -H pip3 install pip --upgrade
 mkdir --parents ~/.config/pip
