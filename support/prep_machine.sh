55c8c22927992f326d26c4ce507a765ad3dafae0
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-03 11:54:51 +0300

    scripts arranged

--- support/prep_machine.sh
+++ support/prep_machine.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-: '
+# This script does the basic stuff on the machine
 
-This script does the basic stuff on the machine
-
-After this script is done download any packages you want.
-Install requirements using
-$ pip3 install -r requirements.txt
-'
-
-ssh MarksHome "sudo apt-get install python-pip python-dev python3-pip python3-dev git apt-file awscli s3cmd s3fs zip tree jq parallel mdadm lrzip bc sqlite3 pipemeter"
-ssh MarksHome "apt-get update"
-ssh MarksHome "apt-get dist-upgrade"
+sudo apt -y update
+sudo apt -y dist-upgrade
+sudo apt -y auto-remove
 # a reboot may be necessary if a new kernel was installed
-ssh MarksHome "apt install python3-pip python3-dev python-pip python-dev"
-ssh MarksHome "sudo -H pip install pip --upgrade"
-ssh MarksHome "sudo -H pip3 install pip --upgrade"
-scp -r ~/.pgpass ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs MarksHome:~
-ssh MarksHome "mkdir --parents ~/.config/pip"; scp ~/.config/pip/pip.conf MarksHome:~/.config/pip
-ssh MarksHome "mkdir --parents /root/.config/pip"; scp ~/.config/pip/pip.conf MarksHome:/root/.config/pip
+sudo apt -y install python-pip python-dev python3-pip python3-dev git apt-file awscli s3cmd s3fs zip tree jq parallel mdadm lrzip bc sqlite3 pipemeter awscli postgresql-client-common postgresql-client-9.5
+sudo -H pip install pip --upgrade
+sudo -H pip3 install pip --upgrade
+mkdir --parents ~/.config/pip
+sudo mkdir --parents /root/.config/pip
 # set the hostname of the machine (it's usually hostname is ip-XXX-XXX-XXX-XXX).
-ssh MarksHome "echo MarksHome  > /etc/hostname"
+sudo sh -c "echo MarksHome  > /etc/hostname"
 # set quiet login using:
-ssh MarksHome "touch ~/.hushlogin"
+touch ~/.hushlogin
