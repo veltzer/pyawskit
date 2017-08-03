40f076c430cca1a366cb4abff42873ac20147678
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-03 17:20:47 +0300

    better script

--- scripts/from_mine.sh
+++ scripts/from_mine.sh
@@ -3,6 +3,7 @@
 # HOST_NAME=MarksHome
 HOST_NAME=MarksZuhause
 scp -r ~/.pgpass ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs $HOST_NAME: > /dev/null
+ssh $HOST_NAME "mkdir --parents ~/.config/pip"
 scp ~/.config/pip/pip.conf $HOST_NAME:~/.config/pip > /dev/null
 scp -r ~/.bashrc.d ~/.bashy ~/.bashrc $HOST_NAME: > /dev/null
 scp -r ~/.hushlogin $HOST_NAME: > /dev/null
