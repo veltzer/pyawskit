271eb08e88b1f381375ac4c18fc6ead9f99d97b5
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-03 13:44:28 +0300

    better scripts

--- scripts/from_mine.sh
+++ scripts/from_mine.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ue
 
-scp -r ~/.pgpass ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs MarksHome:~ > /dev/null
+scp -r ~/.pgpass ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs MarksHome: > /dev/null
 scp ~/.config/pip/pip.conf MarksHome:~/.config/pip > /dev/null
-# scp ~/.config/pip/pip.conf MarksHome:/root/.config/pip
+scp -r ~/.bashrc.d ~/.bashy ~/.bashrc MarksHome: > /dev/null
+# scp ~/.config/pip/pip.conf MarksHome:/root/.config/pip > /dev/null
