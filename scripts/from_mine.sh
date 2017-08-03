7855de42b73b45e3759acb836f729845c9fc185a
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-03 15:49:04 +0300

    better scripts

--- scripts/from_mine.sh
+++ scripts/from_mine.sh
@@ -3,4 +3,5 @@
 scp -r ~/.pgpass ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs MarksHome: > /dev/null
 scp ~/.config/pip/pip.conf MarksHome:~/.config/pip > /dev/null
 scp -r ~/.bashrc.d ~/.bashy ~/.bashrc MarksHome: > /dev/null
+scp -r ~/.hushlogin MarksHome: > /dev/null
 # scp ~/.config/pip/pip.conf MarksHome:/root/.config/pip > /dev/null
--- scripts/prep_machine.sh
+++ scripts/prep_machine.sh
@@ -9,15 +9,15 @@ sudo apt $APT_QUIET_FLAGS dist-upgrade
 sudo apt $APT_QUIET_FLAGS auto-remove
 # a reboot may be necessary if a new kernel was installed
 sudo apt $APT_QUIET_FLAGS install python-pip python-dev python3-pip python3-dev git apt-file awscli s3cmd s3fs zip tree jq parallel mdadm lrzip bc sqlite3 pipemeter awscli postgresql-client-common postgresql-client-9.5 virtualenv virtualenvwrapper dialog
+
 sudo -H pip install pip --upgrade
 sudo -H pip3 install pip --upgrade
+
 mkdir --parents ~/.config/pip
 sudo mkdir --parents /root/.config/pip
+
 # set the hostname of the machine (it's usually hostname is ip-XXX-XXX-XXX-XXX).
 HOST_NAME="MarksZuhause"
 sudo sh -c "echo 127.0.0.1 $HOST_NAME >> /etc/hosts"
 sudo sh -c "echo $HOST_NAME > /etc/hostname"
 sudo hostname "$HOST_NAME"
-# TBD: change /etc/hosts
-# set quiet login
-touch ~/.hushlogin
