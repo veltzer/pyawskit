bee24376efa2d20441f77ca388efa5eb889e7979
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-06 13:50:02 +0300

    better code for bashy

--- scripts/from_mine.sh
+++ scripts/from_mine.sh
@@ -5,6 +5,6 @@ HOST_NAME=MarksZuhause
 scp -r ~/.pgpass ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs $HOST_NAME: > /dev/null
 ssh $HOST_NAME "mkdir --parents ~/.config/pip"
 scp ~/.config/pip/pip.conf $HOST_NAME:~/.config/pip > /dev/null
-scp -r ~/.bashrc.d ~/.bashy ~/.bashrc $HOST_NAME: > /dev/null
+scp -r ~/.bashrc ~/.bashy $HOST_NAME: > /dev/null
 scp -r ~/.hushlogin $HOST_NAME: > /dev/null
 # scp ~/.config/pip/pip.conf $HOST_NAME:/root/.config/pip > /dev/null
