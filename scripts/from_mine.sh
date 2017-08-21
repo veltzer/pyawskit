263f8d95fc987f4ec590fb6f3dc8179213757e67
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-21 19:31:41 +0300

    added bash completions

--- scripts/from_mine.sh
+++ scripts/from_mine.sh
@@ -2,7 +2,7 @@
 
 HOST_NAME=MarksHome
 # HOST_NAME=MarksZuhause
-scp -r ~/.api-keys.json ~/.pgpass ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs $HOST_NAME: > /dev/null
+scp -r ~/.api-keys.json ~/.pgpass ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs ~/.bash_completion.d $HOST_NAME: > /dev/null
 ssh $HOST_NAME "mkdir --parents ~/.config/pip"
 scp ~/.config/pip/pip.conf $HOST_NAME:~/.config/pip > /dev/null
 scp -r ~/.bashrc ~/.bashy $HOST_NAME: > /dev/null
