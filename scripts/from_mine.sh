11f7224bf072c5ebed662739dffc8c2a600d28d4
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-13 11:56:09 +0300

    more stuff

--- scripts/from_mine.sh
+++ scripts/from_mine.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -ue
 
+HOST_NAME=MarksZuhause
 HOST_NAME=MarksHome
-# HOST_NAME=MarksZuhause
-scp -r ~/.api-keys.json ~/.pgpass ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs ~/.bash_completion.d $HOST_NAME: > /dev/null
-ssh $HOST_NAME "mkdir --parents ~/.config/pip"
-scp ~/.config/pip/pip.conf $HOST_NAME:~/.config/pip > /dev/null
-scp -r ~/.bashrc ~/.bashy $HOST_NAME: > /dev/null
-scp -r ~/.hushlogin $HOST_NAME: > /dev/null
-# scp ~/.config/pip/pip.conf $HOST_NAME:/root/.config/pip > /dev/null
+
+# secrets
+scp -r ~/.api-keys.json ~/.pgpass ~/.netrc ~/.aws ~/.ssh ~/.s3cfg ~/.gitconfig ~/.passwd-s3fs "$HOST_NAME:" > /dev/null
+
+# pip config
+ssh "$HOST_NAME" "mkdir --parents ~/.config/pip" > /dev/null
+scp ~/.config/pip/pip.conf "$HOST_NAME:"~/.config/pip > /dev/null
+
+# bash config
+scp -r ~/.bashrc ~/.bashy ~/.bash_completion.d "$HOST_NAME:" > /dev/null
+
+# misc
+scp -r ~/.hushlogin "$HOST_NAME:" > /dev/null
