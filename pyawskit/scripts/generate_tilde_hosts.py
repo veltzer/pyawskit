4bebf4242315dc364358f79f28490caebd0a7ccf
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-21 06:56:50 +0300

    better documentation

--- pyawskit/scripts/generate_tilde_hosts.py
+++ pyawskit/scripts/generate_tilde_hosts.py
@@ -6,8 +6,8 @@ import click
 @click.option("--all-instances/--filter", default=False, help="filter or add all instances")
 def main(all_instances):
     """
-    This script will update ~/.aws/config file with the names of your machines.
-    Notice that you must hold all of your .pem files in ~/.aws/keys
+    This script will update ~/.hosts file with the names of your machines.
+    You must use something like libnss_homehosts to use this file for each app.
     """
     setup()
     update_file(filename="~/.hosts", pattern="{ip} {host}\n", do_all=all_instances)
