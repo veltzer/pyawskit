61f3c4ae97179a6938aab87e086f5bc558449875
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-18 12:17:19 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pyawskit',
-    version='0.1.52',
+    version='0.1.53',
     description='pyawskit is a collection of utilities to help interact with aws',
     long_description='pyawskit is a collection of utilities to help interact with aws',
     url='https://veltzer.github.io/pyawskit',
