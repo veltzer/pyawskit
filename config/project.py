22df27cb22a673f46f0163b894aec769e556174a
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-27 00:33:14 +0300

    better project config

--- config/project.py
+++ config/project.py
@@ -26,7 +26,7 @@ project_keywords = [
     "line",
 ]
 project_license = "MIT"
-project_year_started = "2016"
+project_year_started = 2016
 project_description = project_short_description
 project_platforms = [
     "python3",
