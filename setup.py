5507ef8907532678dd346ac6032c28c97f75262f
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-28 08:35:44 +0300

    better templates

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setuptools.setup(
         'pytconf',
     ],
     extras_require={
-    }},
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
