fece280296f95787d931a995aa0d583abdd3cdc0
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:11:18 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -3,48 +3,37 @@ import config.project
 package_name = config.project.project_name
 
 console_scripts = [
-    'pyawskit=pyawskit.main:main',
-]
-
-setup_requires = [
+    "pyawskit=pyawskit.main:main",
 ]
 
 run_requires = [
-    'pylogconf',
-    'pyfakeuse',
-    'pypipegzip',
-    'tqdm',
-    'requests',
-    'boto3',
-    'pymount',
-    'ujson',
-    'sultan',
-    'pytconf',
+    "pylogconf",
+    "pyfakeuse",
+    "pypipegzip",
+    "tqdm",
+    "requests",
+    "boto3",
+    "pymount",
+    "ujson",
+    "sultan",
+    "pytconf",
 ]
 
 test_requires = [
-    'pylint',
-    'pytest',
-    'pytest-cov',
-    'pyflakes',
-    'flake8',
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "pyflakes",
+    "flake8",
 ]
 
 dev_requires = [
-    'pyclassifiers',
-    'pypitools',
-    'pydmt',
-    'Sphinx',
-    'pymakehelper',
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
+    "Sphinx",
+    "pymakehelper",
 ]
-
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-python_requires = ">=3.9"
-
-extras_require = {
-}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
