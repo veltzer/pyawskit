79a0453441b16341d5a676cbc4ed40569d9e412a
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-16 20:14:10 +0300

    new version

--- pyawskit/static.py
+++ pyawskit/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.1.63"
+VERSION_STR = "0.1.64"
 DESCRIPTION = "pyawskit is a collection of utilities to help interact with aws"
 APP_NAME = "pyawskit"
 LOGGER_NAME = "pyawskit"
