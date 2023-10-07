c912a47c7d6ddbd340f52a3c72636e924971402b
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-07 17:46:09 +0300

    new version

--- pyawskit/static.py
+++ pyawskit/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.1.80"
+VERSION_STR = "0.1.81"
 DESCRIPTION = "Pyawskit is your AWS Swiss Army Knife"
 APP_NAME = "pyawskit"
 LOGGER_NAME = "pyawskit"
