1285ec56aadb1d0c9cdb2361982fe6655a941546
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-07 17:14:30 +0300

    more reorg

--- pyawskit/devices.py
+++ pyawskit/devices.py
@@ -1,3 +1,5 @@
+import logging
+import os
 import subprocess
 import pymount.mgr
 
@@ -70,3 +72,102 @@ def mount_disks() -> None:
         pyawskit.common.erase_partition_table(disk=disk)
         pyawskit.common.format_device(disk=disk)
         pyawskit.common.mount_disk(disk=disk, folder=folder)
+
+
+def unify_disks() -> None:
+    """
+    unify disks of a machine in raid 0
+
+    This script is derived from the following bash script:
+    https://gist.github.com/joemiller/6049831
+
+    NOTES:
+    - python has no support for 'mount' and 'umount' system
+    calls. the result is that we use the command line tools
+    to do mounting and un mounting.
+    References:
+    - http://stackoverflow.com/questions/1667257/how-do-i-mount-a-filesystem-using-python
+
+    References:
+    - http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/raid-config.html
+    - http://www.tecmint.com/create-raid0-in-linux/
+    - http://dev.bizo.com/2012/07/mdadm-device-or-resource-busy.html
+
+    TODO:
+    - make this script work in parallel. But for the dd(1) and mkfs(1) parts.
+    """
+
+    logger = logging.getLogger(__name__)
+    logger.info("looking for disks...")
+    disks = pyawskit.common.get_disks()
+    assert len(disks) > 0, "found no disks"
+
+    logger.info(f"got {len(disks)} disks {str(disks)}")
+
+    check_unmounted(logger, disks)
+
+    logger.info(f"checking if device exists [{ConfigWork.device_file}]")
+    if os.path.exists(ConfigWork.device_file):
+        logger.info(f"stopping md on the current device [{ConfigWork.device_file}]")
+        subprocess.check_call([
+            ConfigWork.mdadm_binary,
+            "--stop",
+            ConfigWork.device_file,
+        ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        logger.info(f"stopped md on [{ConfigWork.device_file}] successfully...")
+    else:
+        logger.info(f"no md [{ConfigWork.device_file}], good")
+
+    for disk in disks:
+        pyawskit.common.erase_partition_table(disk)
+    pyawskit.common.reread_partition_table()
+
+    create_new_device(logger, disks)
+
+    if ConfigWork.start_stop_queue:
+        subprocess.check_call([
+            "/sbin/udevadm",
+            "control",
+            "--start-exec-queue",
+        ])
+
+    # removed writing /etc/mdadm because it was causing problems
+    if ConfigWork.write_etc_mdadm:
+        logger.info(f"writing the details of the new device in [{ConfigWork.mdadm_config_file}]")
+        with open(ConfigWork.mdadm_config_file, "at") as mdadm_handle:
+            subprocess.check_call([
+                ConfigWork.mdadm_binary,
+                "--detail",
+                "--scan",
+                "--verbose",
+            ], stdout=mdadm_handle, stderr=subprocess.DEVNULL)
+
+    pyawskit.common.format_device(ConfigWork.device_file, label=ConfigWork.name_of_raid_device)
+
+    pyawskit.common.mount_disk(disk=ConfigWork.device_file, folder=ConfigWork.mount_point)
+
+    if ConfigWork.add_line_to_fstab:
+        logger.info(f"checking if need to add line to [{ConfigWork.fstab_filename}] for mount on reboot...")
+        line_to_add = " ".join([
+            # ConfigWork.device_file,
+            f"LABEL={ConfigWork.name_of_raid_device}",
+            ConfigWork.mount_point,
+            ConfigWork.file_system_type,
+            "defaults",
+            "0",
+            "0",
+        ])
+        found_line_to_add = False
+        with open(ConfigWork.fstab_filename) as file_handle:
+            for line in file_handle:
+                line = line.rstrip()
+                if line == line_to_add:
+                    found_line_to_add = True
+                    break
+        if found_line_to_add:
+            logger.info("found the line to add. not doing anything...")
+        else:
+            logger.info("adding line to [%s]", ConfigWork.fstab_filename)
+            with open(ConfigWork.fstab_filename, "at") as file_handle:
+                file_handle.write(line_to_add + "\n")
+    # create ubuntu folder and chown it to ubuntu
--- pyawskit/main.py
+++ pyawskit/main.py
@@ -1,4 +1,3 @@
-import logging
 import multiprocessing
 import subprocess
 import sys
@@ -16,9 +15,8 @@ from pytconf import register_main, config_arg_parse_and_launch, register_endpoin
 import pyawskit.common
 from pyawskit.aws import ProcessData, request_spot_instances, tag_resources, poll_instances_till_done, wait_for_ssh, \
     attach_disks
-from pyawskit.common import update_etc_hosts, update_ssh_config, update_file, do_hush_login, wait_net_service, \
-    get_disks, erase_partition_table, reread_partition_table, format_device, mount_disk
-from pyawskit.configs import ConfigFilter, ConfigName, ConfigWork, ConfigAwsCodeartifactNpm, ConfigAwsCodeartifactPip
+from pyawskit.common import update_etc_hosts, update_ssh_config, update_file, do_hush_login, wait_net_service
+from pyawskit.configs import ConfigFilter, ConfigName, ConfigAwsCodeartifactNpm, ConfigAwsCodeartifactPip
 
 from pyawskit.static import APP_NAME, VERSION_STR, DESCRIPTION
 from pyawskit.utils import object_exists, compress_one_file, print_exception
@@ -26,8 +24,6 @@ from pyawskit.utils import object_exists, compress_one_file, print_exception
 import pyawskit.aws_codeartifact_npm_env_config_code
 import pyawskit.aws_codeartifact_pip_env_config_code
 import pyawskit.aws_ecr_login_code
-
-from pyawskit.devices import create_new_device, check_unmounted
 
 
 @register_endpoint(
@@ -263,106 +259,7 @@ def prep_machine(name: str) -> None:
     description="Shows all the disks you have on an AWS machine",
 )
 def show_disks() -> None:
-    print(get_disks())
-
-
-def unify_disks() -> None:
-    """
-    unify disks of a machine in raid 0
-
-    This script is derived from the following bash script:
-    https://gist.github.com/joemiller/6049831
-
-    NOTES:
-    - python has no support for 'mount' and 'umount' system
-    calls. the result is that we use the command line tools
-    to do mounting and un mounting.
-    References:
-    - http://stackoverflow.com/questions/1667257/how-do-i-mount-a-filesystem-using-python
-
-    References:
-    - http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/raid-config.html
-    - http://www.tecmint.com/create-raid0-in-linux/
-    - http://dev.bizo.com/2012/07/mdadm-device-or-resource-busy.html
-
-    TODO:
-    - make this script work in parallel. But for the dd(1) and mkfs(1) parts.
-    """
-
-    logger = logging.getLogger(__name__)
-    logger.info("looking for disks...")
-    disks = get_disks()
-    assert len(disks) > 0, "found no disks"
-
-    logger.info(f"got {len(disks)} disks {str(disks)}")
-
-    check_unmounted(logger, disks)
-
-    logger.info(f"checking if device exists [{ConfigWork.device_file}]")
-    if os.path.exists(ConfigWork.device_file):
-        logger.info(f"stopping md on the current device [{ConfigWork.device_file}]")
-        subprocess.check_call([
-            ConfigWork.mdadm_binary,
-            "--stop",
-            ConfigWork.device_file,
-        ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        logger.info(f"stopped md on [{ConfigWork.device_file}] successfully...")
-    else:
-        logger.info(f"no md [{ConfigWork.device_file}], good")
-
-    for disk in disks:
-        erase_partition_table(disk)
-    reread_partition_table()
-
-    create_new_device(logger, disks)
-
-    if ConfigWork.start_stop_queue:
-        subprocess.check_call([
-            "/sbin/udevadm",
-            "control",
-            "--start-exec-queue",
-        ])
-
-    # removed writing /etc/mdadm because it was causing problems
-    if ConfigWork.write_etc_mdadm:
-        logger.info(f"writing the details of the new device in [{ConfigWork.mdadm_config_file}]")
-        with open(ConfigWork.mdadm_config_file, "at") as mdadm_handle:
-            subprocess.check_call([
-                ConfigWork.mdadm_binary,
-                "--detail",
-                "--scan",
-                "--verbose",
-            ], stdout=mdadm_handle, stderr=subprocess.DEVNULL)
-
-    format_device(ConfigWork.device_file, label=ConfigWork.name_of_raid_device)
-
-    mount_disk(disk=ConfigWork.device_file, folder=ConfigWork.mount_point)
-
-    if ConfigWork.add_line_to_fstab:
-        logger.info(f"checking if need to add line to [{ConfigWork.fstab_filename}] for mount on reboot...")
-        line_to_add = " ".join([
-            # ConfigWork.device_file,
-            f"LABEL={ConfigWork.name_of_raid_device}",
-            ConfigWork.mount_point,
-            ConfigWork.file_system_type,
-            "defaults",
-            "0",
-            "0",
-        ])
-        found_line_to_add = False
-        with open(ConfigWork.fstab_filename) as file_handle:
-            for line in file_handle:
-                line = line.rstrip()
-                if line == line_to_add:
-                    found_line_to_add = True
-                    break
-        if found_line_to_add:
-            logger.info("found the line to add. not doing anything...")
-        else:
-            logger.info("adding line to [%s]", ConfigWork.fstab_filename)
-            with open(ConfigWork.fstab_filename, "at") as file_handle:
-                file_handle.write(line_to_add + "\n")
-    # create ubuntu folder and chown it to ubuntu
+    print(pyawskit.common.get_disks())
 
 
 @register_main(
