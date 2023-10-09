4cc747399942d5a8d39b3bee3a5d5fdd5c18d3b7
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-09 22:43:55 +0300

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pyawskit",
-    version="0.1.85",
+    version="0.1.86",
     packages=[
         "pyawskit",
     ],
