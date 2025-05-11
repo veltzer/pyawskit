4b494206a255fee4800ca1901ff12d31ac313b2a
Mark Veltzer <mark.veltzer@gmail.com>
2025-05-11 09:22:41 +0300

    license fix

--- setup.py
+++ setup.py
@@ -59,7 +59,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.12",
         "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License",
     ],
     entry_points={"console_scripts": [
         "pyawskit=pyawskit.main:main",
