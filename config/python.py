f8960191549cc74103d458e11d1d2819c7465f12
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 21:46:11 +0300

    more

--- config/python.py
+++ config/python.py
@@ -1,8 +1,8 @@
 """ python deps for this project """
 
-console_scripts: list[str] = [
-    "pyawskit=pyawskit.main:main",
-]
+scripts: dict[str,str] = {
+    "pyawskit": "pyawskit.main:main",
+}
 config_requires: list[str] = [
     "pyclassifiers",
 ]
---
+++ pyawskit/static.py
@@ -0,0 +1,5 @@
+""" version which can be consumed from within the module """
+VERSION_STR = "0.1.87"
+DESCRIPTION = "Pyawskit is your AWS Swiss Army Knife"
+APP_NAME = "pyawskit"
+LOGGER_NAME = "pyawskit"
